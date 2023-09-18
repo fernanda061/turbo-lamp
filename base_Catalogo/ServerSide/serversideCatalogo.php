009d204fa62f1bdf2d92f865a131696a0c30e002
fernanda061 <144829533+fernanda061@users.noreply.github.com>
2023-09-18 11:47:15 -0600

    Base_Catalogo
    Ejemplo de base datos con registros grandes

---
+++ base_Catalogo/ServerSide/serversideCatalogo.php
@@ -0,0 +1,5 @@
+<?php
+require 'serverside.php';
+$table_data->get('vista_catalogo','id',array('id', 'codigo','breve','larga','u_medida','libre_utilizacion','almacen'));
+
+?>
---
+++ base_Catalogo/ServerSide/serversideConexion.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST_SS","localhost");
+define("DATABASE_SS","catalogo2");
+define("USER_SS","root");
+define("PASSWORD_SS","");
+?>
